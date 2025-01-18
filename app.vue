3558bf35e254b72e437de95661d33dbed4a9ecf0
yosuke-tomisawa <bhav.urs@gmail.com>
2025-01-18 16:01:36 +0900

    ✨ new projects

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="/">top</a></li>
+        <li><a href="about">about</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div>
+    <NuxtPage />
+  </div>
+</template>

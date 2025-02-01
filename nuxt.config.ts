2dabd210c8caec1e74d7618e1c065a92715b89b3
yosuke-tomisawa <bhav.urs@gmail.com>
2025-02-01 23:50:36 +0900

    ⚡️ cms

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const { API_KEY, API_URL } = process.env;
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  runtimeConfig: {
+    apiKey: API_KEY,
+    public: {
+      apiUrl: API_URL,
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,48 @@
 <template>
   <div>
     <h1>Top Page</h1>
+    <div>
+      <img v-if="image" :src="imageUrl" :alt="name" :class="imageClass" />
+    </div>
+    <div>
+      <p>{{ name }}</p>
+      <p>{{ body }}</p>
+      <p>{{ price }}円</p>
+    </div>
   </div>
 </template>
+<script>
+// import axios from "axios";
+export default {
+  // async asyncData({ $config }) {
+  //   const { data } = await axios.get(`${$config.apiUrl}/menu`, {
+  //     headers: { "X-API-KEY": $config.apiKey },
+  //   });
+  //   return {
+  //     items: data.contents,
+  //   };
+  // },
+  props: {
+    image: {
+      type: Object,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
